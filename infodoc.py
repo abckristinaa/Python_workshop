3c0c21dd859ee5418e4ae6f23f40432b4c1e8543
Kristina <abckristinaa@gmail.com>
2019-09-07 17:09:51 +0300

    Задача Палиндром

---
+++ infodoc.py
@@ -0,0 +1,12 @@
+
+def info(x):
+    '''this function returns the list of availiable methods with short description'''
+
+    methods = dir(x)
+    for method in methods:
+        try:
+            docs = getattr(x, method).__doc__.split('\n')[0]
+        except:
+            docs = getattr(x, method).__doc__
+        print(method.ljust(20), docs)
+
